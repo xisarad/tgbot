160a4542ba2d4ca92903303630482bfa54d0fd0b
xisarad <xisarad@yandex.ru>
2025-12-15 21:43:35 +0300

    Добавлены комментарии в код и описание бота в README

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):
+    # Эхо-бот: повторяет сообщение пользователя
     bot.send_message(message.chat.id, message.text)
 
 if __name__ == '__main__':
-     bot.infinity_polling()
+    bot.infinity_polling()
