837abb42c34b8ae41b464b66755bb6cc6f2fa5e9
xisarad <xisarad@yandex.ru>
2025-12-15 21:51:57 +0300

    Изменён формат ответа бота: теперь отвечает 'Вы написали: {текст}'

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):
-    # Эхо-бот: повторяет сообщение пользователя
-    bot.send_message(message.chat.id, message.text)
+    # Отвечаем в формате: Вы написали: {текст}
+    response = f"Вы написали: {message.text}"
+    bot.send_message(message.chat.id, response)
 
 if __name__ == '__main__':
     bot.infinity_polling()
